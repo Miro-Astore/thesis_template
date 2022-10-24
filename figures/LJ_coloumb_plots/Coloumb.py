3785280c7e97585f4b076b3773a2b18a6437d2c2
miro-astore <miro.astore@gmail.com>
2022-10-24 18:27:45 +1100

    progress toward stripped down template

--- figures/LJ_coloumb_plots/Coloumb.py
+++
@@ -1,8 +0,0 @@
-import numpy as np 
-import matplotlib.pyplot as plt
-q = 0.01
-x = np.linspace(0.00964,0.03,1000)
-y = q/x
-plt.plot(x,y,'k')
-plt.savefig('Coloumb.pdf')
-plt.show()
--- figures/LJ_coloumb_plots/LJ.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-import matplotlib.pyplot as plt
-sigma = 0.01
-epsilon = 0.003
-x = np.linspace(0.00964,0.03,1000)
-y = epsilon*((sigma/x)**12 - (sigma/x)**6)
-plt.plot(x,y,'k')
-plt.savefig('LJ.pdf')
-plt.show()
